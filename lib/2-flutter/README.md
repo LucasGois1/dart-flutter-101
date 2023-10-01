a7ab042d67d76203b0f6a4f058307873f83245f1
Lucas Gois <lucassampaiodegois1@gmail.com>
2023-10-01 10:32:54 -0300

    refactor: components

---
+++ lib/2-flutter/README.md
@@ -0,0 +1,19 @@
+# O que é o MaterialApp?
+O MaterialApp é um widget que define a estrutura básica de um aplicativo Flutter seguindo as diretrizes de design do Material Design, um sistema de design desenvolvido pelo Google. O Material Design fornece diretrizes de design consistentes para criar aplicativos com uma aparência moderna e coerente.
+
+## Recursos oferecidos pelo MaterialApp:
+ - Tema Material: O MaterialApp define um tema de aplicativo que define a aparência e o estilo padrão do aplicativo, incluindo cores, tipografia, espaçamento, etc. Isso ajuda a garantir que seu aplicativo siga as diretrizes do Material Design.
+- Roteamento: O MaterialApp permite configurar o roteamento de páginas em seu aplicativo, tornando mais fácil a navegação entre telas e a gestão de rotas.
+- Internationalization: Suporte a internacionalização integrado, facilitando a tradução do seu aplicativo para diferentes idiomas.
+- Manuseio de Gestos: O MaterialApp configura gestos padrão, como gestos de deslizamento para voltar, que são comuns nos aplicativos Android.
+- Widgets de Scaffold: O MaterialApp fornece um ponto de entrada para a criação de Scaffold, que é um widget fundamental para criar a estrutura de uma tela no estilo Material Design, incluindo a barra de aplicativos, o menu de navegação, o corpo da tela e muito mais.
+- Integração com Temas Escuros e Claros: O MaterialApp suporta temas claros e escuros, permitindo que seu aplicativo se ajuste automaticamente às preferências do usuário ou às configurações do sistema.
+- Manuseio de Fontes e Estilos de Texto: O MaterialApp gerencia as configurações de fonte e estilo de texto do aplicativo, garantindo uma aparência consistente em todo o aplicativo.
+- Por que o MaterialApp é usado? 
+
+## O MaterialApp é usado principalmente por duas razões principais:
+
+- Adoção das diretrizes do Material Design: Se você deseja criar um aplicativo que siga as diretrizes de design do Material Design, o MaterialApp fornece um ponto de partida fácil e consistente. Ele configura automaticamente muitos aspectos visuais e comportamentais do seu aplicativo para se alinhar com o Material Design.
+- Facilita a configuração do aplicativo: O MaterialApp simplifica muitas tarefas comuns, como a configuração de temas, roteamento e internacionalização, tornando mais fácil para os desenvolvedores criar aplicativos Flutter completos e bem projetados.
+
+Em resumo, o MaterialApp é usado para configurar e personalizar a aparência, o comportamento e a estrutura básica de um aplicativo Flutter, seguindo as diretrizes de design do Material Design e tornando o desenvolvimento de aplicativos mais eficiente e coeso. É uma escolha comum para muitos aplicativos Flutter, especialmente aqueles que desejam seguir o design moderno do Material Design.
--- lib/2-flutter/basic.dart
+++ lib/2-flutter/basic.dart
@@ -8,91 +8,94 @@ class HomeBasic extends StatefulWidget {
   State<HomeBasic> createState() => _HomeBasicState();
 }
 
-class _HomeBasicState extends State<HomeBasic> with TickerProviderStateMixin {
+class _HomeBasicState extends State<HomeBasic>
+    with SingleTickerProviderStateMixin {
+  // Ticker é uma classe que permite que você receba notificações de cada frame.
+
   final List<EmailCard> emails = [
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
     EmailCard(
       sentFrom: 'John Doe',
       subject: "New Flutter release",
       body: ""
-          "Flutter 2.5 is now available! This release includes the following new features and improvements",
+          "Flutter 3.13 is now available! This release includes the following new features and improvements",
     ),
   ];
 
@@ -105,25 +108,31 @@ class _HomeBasicState extends State<HomeBasic> with TickerProviderStateMixin {
     super.initState();
 
     _animationController = AnimationController(
-      duration: const Duration(milliseconds: 100),
+      duration: const Duration(milliseconds: 200),
       vsync: this,
-    )..forward();
+    )..forward(); // Ja definir o valor inicial do controller
 
     _animation = CurvedAnimation(
+      // Tipo do algoritmo de animação que será utilizado
       parent: _animationController,
       curve: Curves.fastEaseInToSlowEaseOut,
     );
 
     _scrollController = ScrollController();
-    _scrollController.addListener(() {
-      if (_scrollController.position.userScrollDirection ==
-          ScrollDirection.reverse) {
-        _animationController.reverse();
-      } else if (_scrollController.position.userScrollDirection ==
-          ScrollDirection.forward) {
-        _animationController.forward();
-      }
-    });
+    _scrollController.addListener(updateAnimationState);
+    // Quando um evento de scroll é disparado, o listener é chamado
+  }
+
+  void updateAnimationState() {
+    // Verifica o estado do scroll e atualiza o estado da animação
+
+    if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.reverse) {
+      _animationController.reverse();
+    } else if (_scrollController.position.userScrollDirection ==
+        ScrollDirection.forward) {
+      _animationController.forward();
+    }
   }
 
   @override
@@ -143,97 +152,173 @@ class _HomeBasicState extends State<HomeBasic> with TickerProviderStateMixin {
       ),
       home: SafeArea(
         child: Scaffold(
-          bottomNavigationBar: Container(
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 4,
-                  blurRadius: 4,
-                  offset: const Offset(0, 4),
+          appBar: AppBar(
+            backgroundColor: const Color.fromRGBO(250, 250, 250, 1),
+            elevation: 0,
+            toolbarHeight: 80,
+            flexibleSpace: const SearchMail(),
+          ),
+          body: EmailList(
+            scrollController: _scrollController,
+            emails: emails,
+          ),
+          floatingActionButton: const WriteMailButton(),
+          bottomNavigationBar: NavigationBar(
+            animation: _animation,
+            emails: emails,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WriteMailButton extends StatelessWidget {
+  const WriteMailButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+        ),
+      ),
+      child: const SizedBox(
+        width: 100,
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 10.0,
+            bottom: 10.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.edit),
+              Text("Escrever"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class EmailList extends StatelessWidget {
+  const EmailList({
+    super.key,
+    required ScrollController scrollController,
+    required this.emails,
+  }) : _scrollController = scrollController;
+
+  final ScrollController _scrollController;
+  final List<EmailCard> emails;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromRGBO(250, 250, 250, 1),
+      child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          controller: _scrollController,
+          child: Column(
+            children: emails,
+          )),
+    );
+  }
+}
+
+class SearchMail extends StatelessWidget {
+  const SearchMail({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(14.0),
+      child: Row(
+        children: [
+          Flexible(
+            child: SearchBar(
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(4),
+                ),
+              ),
+              hintText: 'Search in mail',
+              hintStyle: MaterialStateProperty.all(
+                const TextStyle(
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+              padding: MaterialStateProperty.all(
+                const EdgeInsets.only(left: 10),
+              ),
+              leading: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.menu),
+              ),
+              trailing: const [
+                Padding(
+                  padding: EdgeInsets.only(right: 10.0),
+                  child: CircleAvatar(
+                    child: Icon(Icons.person_outline_rounded),
+                  ),
                 ),
               ],
             ),
-            child: SizeTransition(
-              sizeFactor: _animation,
-              child: BottomNavigationBar(
-                backgroundColor: Colors.white,
-                items: [
-                  BottomNavigationBarItem(
-                    icon: Badge(
-                      backgroundColor: Colors.red,
-                      label: Text(emails.length.toString()),
-                      child: const Icon(Icons.mail),
-                    ),
-                    label: "",
-                  ),
-                  const BottomNavigationBarItem(
-                    label: "",
-                    icon: Icon(Icons.videocam_outlined),
-                  )
-                ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NavigationBar extends StatelessWidget {
+  const NavigationBar({
+    super.key,
+    required Animation<double> animation,
+    required this.emails,
+  }) : _animation = animation;
+
+  final Animation<double> _animation;
+  final List<EmailCard> emails;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 4,
+            blurRadius: 4,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: SizeTransition(
+        sizeFactor: _animation,
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          items: [
+            BottomNavigationBarItem(
+              icon: Badge(
+                backgroundColor: Colors.red,
+                label: Text(emails.length.toString()),
+                child: const Icon(Icons.mail),
               ),
+              label: "",
             ),
-          ),
-          appBar: AppBar(
-            backgroundColor: Color.fromRGBO(250, 250, 250, 1),
-            elevation: 0,
-            toolbarHeight: 80,
-            flexibleSpace: Padding(
-              padding: const EdgeInsets.all(14.0),
-              child: Row(
-                children: [
-                  Flexible(
-                      child: SearchBar(
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(4))),
-                    hintText: 'Search in mail',
-                    padding: MaterialStateProperty.all(
-                        const EdgeInsets.only(left: 10)),
-                    leading: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.menu),
-                    ),
-                    trailing: const [
-                      Padding(
-                        padding: EdgeInsets.only(right: 10.0),
-                        child: CircleAvatar(
-                          child: Icon(Icons.person),
-                        ),
-                      ),
-                    ],
-                  )),
-                ],
-              ),
-            ),
-          ),
-          body: Container(
-            color: const Color.fromRGBO(250, 250, 250, 1),
-            child: SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                controller: _scrollController,
-                child: Column(
-                  children: emails,
-                )),
-          ),
-          // floatingActionButtonLocation: FloatingActionButtonLocation,
-          floatingActionButton: ElevatedButton(
-              onPressed: () {},
-              // rounded button
-              style: ElevatedButton.styleFrom(
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-              ),
-              child: const SizedBox(
-                width: 100,
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [Icon(Icons.edit), Text("Escrever")],
-                  ),
-                ),
-              )),
+            const BottomNavigationBarItem(
+              label: "",
+              icon: Icon(Icons.videocam_outlined),
+            )
+          ],
         ),
       ),
     );
@@ -262,15 +347,16 @@ class EmailCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// Avatar, followed by the author, subject and cliped body, followed by the hour if sent today, and day of week if older and favorite icon
-
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 4.0, right: 12.0),
+            padding: const EdgeInsets.only(
+              left: 4.0,
+              right: 12.0,
+            ),
             child: _avatar,
           ),
           Expanded(
@@ -278,7 +364,10 @@ class EmailCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(left: 4.0, bottom: 8.0),
+                  padding: const EdgeInsets.only(
+                    left: 4.0,
+                    bottom: 8.0,
+                  ),
                   child: Text(
                     _sentFrom,
                     style: const TextStyle(
@@ -288,7 +377,10 @@ class EmailCard extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 4.0, bottom: 8.0),
+                  padding: const EdgeInsets.only(
+                    left: 4.0,
+                    bottom: 8.0,
+                  ),
                   child: Text(
                     _subject,
                     style: const TextStyle(
@@ -298,15 +390,20 @@ class EmailCard extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.only(left: 4.0, bottom: 8.0),
-                  child: Text(_body,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w400,
-                        color: Colors.grey,
-                        fontSize: 14,
-                      )),
+                  padding: const EdgeInsets.only(
+                    left: 4.0,
+                    bottom: 8.0,
+                  ),
+                  child: Text(
+                    _body,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -316,12 +413,15 @@ class EmailCard extends StatelessWidget {
             children: [
               Text(
                 "${_date.hour}:${_date.minute}",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12,
+                ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 35,
               ),
-              Icon(
+              const Icon(
                 Icons.star_border,
                 color: Colors.grey,
               )
